9e31a047f802233e9f91e9c1716dfb94a1c6bd06
aseemgupta <aseemgupta2625@gmail.com>
2020-09-14 04:44:55 +0530

    Updates

---
+++ precache-manifest.e5b26455107ead30e260cc059fe04cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c527f5570cb34880a95a3d134d433eb0",
+    "url": "/skeleton-loader/index.html"
+  },
+  {
+    "revision": "67a49bb5e24482e4b8ee",
+    "url": "/skeleton-loader/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9a5ce3d5ae72f016af3",
+    "url": "/skeleton-loader/static/js/2.1a090f5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/skeleton-loader/static/js/2.1a090f5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67a49bb5e24482e4b8ee",
+    "url": "/skeleton-loader/static/js/main.1209dc71.chunk.js"
+  },
+  {
+    "revision": "1943b89c9192037967ac",
+    "url": "/skeleton-loader/static/js/runtime-main.9d32dad6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/skeleton-loader/static/media/logo.5d5d9eef.svg"
+  }
+]);
