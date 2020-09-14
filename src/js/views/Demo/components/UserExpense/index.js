e04cd2c7218daf65d8e0d2f59c763dbb355ccb9b
aseemgupta <aseemgupta2625@gmail.com>
2020-09-14 07:40:59 +0530

    Add dummy card with skeleton animation

---
+++ src/js/views/Demo/components/UserExpense/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import Card from '../../../../components/Card/';
+import SkeletonLoader, { SkeletonLoaderBone } from '../../../../components/SkeletonLoader';
+
+import './index.css'
+
+
+export default class UserExpense extends React.Component {
+
+	render() {
+		let isLoading = true, data = null;
+		let content;
+
+		if (isLoading) {
+			content = <UserExpense_Loader />;
+		} else if (!isLoading && data) {
+			content = (
+				<div className="UserExpense-content">
+					<div className="VirtualCard"></div>
+
+					<div class="UserExpense-details">
+						<div className="UserDetails">
+							<div className="UserDetails-profileImg"></div>
+							<div className="UserDetails-title"></div>
+						</div>
+
+						<div className="title"></div>
+						<div className="expense-amount"></div>
+
+						<div className="title"></div>
+						<div className="description"></div>
+
+						<div className="UserExpense-actions">
+							<div className="UserExpense-actions-btn"></div>
+							<div className="UserExpense-actions-btn"></div>
+						</div>
+					</div>
+				</div>
+			);
+		}
+
+		return (
+			<Card className="UserExpense">
+				{content}
+			</Card>
+		);
+	}
+}
+
+export function UserExpense_Loader() {
+	return (
+		<div className="UserExpense-content UserExpense_Loader">
+			<div className="VirtualCard">
+				<SkeletonLoader>
+					<SkeletonLoaderBone />
+				</SkeletonLoader>
+			</div>
+
+			<SkeletonLoader className="UserExpense-details">
+				<div className="UserDetails">
+					<SkeletonLoaderBone className="UserDetails-profileImg" />
+					<SkeletonLoaderBone className="UserDetails-title" width={200} height={20} />
+				</div>
+
+				<SkeletonLoaderBone className="title" width={140} height={20} />
+				<SkeletonLoaderBone className="description expense-amount" width={100} height={36} />
+
+				<SkeletonLoaderBone className="title" width={100} height={20} />
+				<SkeletonLoaderBone className="description" width="80%" height={24} />
+
+				<div className="UserExpense-actions">
+					<SkeletonLoaderBone className="UserExpense-actions-btn" width={100} height={48} />
+					<SkeletonLoaderBone className="UserExpense-actions-btn" width={100} height={48} />
+				</div>
+			</SkeletonLoader>
+		</div>
+	)
+}
--- src/js/views/Demo/index.js
+++ src/js/views/Demo/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import LayoutA from './components/LayoutA';
-import LayoutB from './components/LayoutB';
+import UserExpense from './components/UserExpense';
 
 import './index.css'
 
@@ -9,11 +8,9 @@ import './index.css'
 export default class Demo extends React.Component {
 
 	render() {
-
 		return (
-			<div className="Home">
-				<LayoutA />
-				<LayoutB />
+			<div className="Demo">
+				<UserExpense />
 			</div>
 		)
 	}
